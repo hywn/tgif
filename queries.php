08d9836a6c903f9aa02e499b73556ae241e122cb
hywn <jh7qbe@virginia.edu>
2020-07-20 17:44:16 -0400

    on viewclasses.html replaced 'avg' with class avg, prof avg

--- queries.php
+++ queries.php
@@ -21,10 +21,16 @@ $a_avgs = function ($grp, $num) use ($db) {
 
 $a_all_classes = function () use ($db) {
 	$r = $db->query('
-		select grp, num, sum(responded), avg(m5), dsc
-			from offerings join evals using (sem, cln)
-			group by grp, num
-			order by sum(responded) desc
+		select
+			grp,
+			num,
+			sum(responded),
+			(sum(m1) + sum(m2)) / (count(m1) + count(m2)) as class_avg,
+			(sum(m3) + sum(m4) + sum(m5)) / (count(m3) + count(m4) + count(m5)) as prof_avg,
+			dsc
+		from offerings join evals using (sem, cln)
+		group by grp, num
+		order by sum(responded) desc
 	');
 	$ar = [];
 	while($row = $r -> fetchArray(SQLITE3_NUM))
