bd9d87731c652f142b368f7be62a8f1f98daa679
hywn <jh7qbe@virginia.edu>
2020-07-20 13:24:22 -0400

    ported viewclasses to html+php combo

--- classes.php
+++ classes.php
@@ -1,25 +1,6 @@
-<style>
-td, th { padding: 0.2em; text-align: right; border: ridge }
-th { text-align: right }
-input { font-size: 2em; padding: 0.2em }
-body { display: flex; flex-direction: column; align-items: center }
-</style>
-
-<h1>all evaluations</h1>
-<input id="input" type="text" placeholder="class name, e.g. 'CS 2150'" autofocus />
-<table>
-	<tr><th>class</th><th>avg. rating</th><th>num. evaluations</th></tr>
-	<tbody id="classes"></tbody>
-</table>
-
-<script>
-const TABLE = document.querySelector('#classes')
-const INPUT = document.querySelector('#input')
-const DATA  = <?php
-
+<?php
 require 'queries.php';
 $classes = $a_all_classes();
-
 
 $stuff = array_map(function($class) {
 
@@ -35,20 +16,4 @@ $stuff = array_map(function($class) {
 }, $classes);
 
 echo json_encode($stuff);
-echo "\n\n";
 ?>
-
-INPUT.addEventListener('input', e => {
-	const input = INPUT.value
-	const grp   = (input.match(/\w+/i) || [''])[0].toUpperCase()
-	const num   = (input.match(/\d+/)  || [''])[0]
-
-	TABLE.innerHTML = DATA
-		.filter(([g, n, disp]) => g.startsWith(grp) && n.startsWith(num))
-		.map(([, , disp]) => disp)
-		.join('')
-})
-
-INPUT.dispatchEvent(new Event('input'))
-
-</script>
