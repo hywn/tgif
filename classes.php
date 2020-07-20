9a0525dc0402fa56d2627d7ff81f97235c967fb1
hywn <jh7qbe@virginia.edu>
2020-07-20 13:44:33 -0400

    process display in html

--- classes.php
+++ classes.php
@@ -2,18 +2,5 @@
 require 'queries.php';
 $classes = $a_all_classes();
 
-$stuff = array_map(function($class) {
-
-	list($grp, $num, $responded, $rating) = $class;
-
-	$rating = number_format($rating, 2);
-
-	$link = "<a href=\"./viewclass.html?class=$grp+$num\">$grp $num</a>";
-	$display = "<tr><td>$link</td><td>$rating</td><td>$responded</td></tr>";
-
-	return [$grp, $num, $display];
-
-}, $classes);
-
-echo json_encode($stuff);
+echo json_encode($classes);
 ?>
