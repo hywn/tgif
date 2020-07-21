9c98693660fd7406ae0838eed4c4141b73af607d
hywn <jh7qbe@virginia.edu>
2020-07-21 08:17:00 -0400

    removed queries.php

--- classes.php
+++ classes.php
@@ -1,6 +1,22 @@
 <?php
-require 'queries.php';
-$classes = $a_all_classes();
+$db = new SQLite3('./data.db');
+
+$r = $db->query('
+	select
+		grp,
+		num,
+		sum(responded),
+		(sum(m1) + sum(m2)) / (count(m1) + count(m2)) as class_avg,
+		(sum(m3) + sum(m4) + sum(m5)) / (count(m3) + count(m4) + count(m5)) as prof_avg,
+		dsc
+	from offerings join evals using (sem, cln)
+	group by grp, num
+	order by sum(responded) desc
+');
+
+$classes = [];
+while($row = $r -> fetchArray(SQLITE3_NUM))
+	array_push($classes, $row);
 
 echo json_encode($classes);
 ?>
